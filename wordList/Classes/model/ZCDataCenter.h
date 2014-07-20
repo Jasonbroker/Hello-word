21200ca16d5049fed212896b2f4fc3596f92a9c7
Jason Zhou 周正昌 <zzhou006@ucr.edu>
2014-07-20 23:58:26 +0800

    model added bug fixed for info,plist.

---
+++ wordList/Classes/model/ZCDataCenter.h
@@ -0,0 +1,21 @@
+//
+//  ZCDataCenter.h
+//  wordList
+//
+//  Created by Jason Zhou on 7/13/14.
+//  Copyright (c) 2014 Jason Zhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZCDataCenter : NSObject
+
+@property (nonatomic, strong) NSMutableArray *unknownWords;
+
+@property (nonatomic, assign) int sectionsLearned;
+
+@property (nonatomic, assign) int wordsLearned;
+
+
+
+@end
